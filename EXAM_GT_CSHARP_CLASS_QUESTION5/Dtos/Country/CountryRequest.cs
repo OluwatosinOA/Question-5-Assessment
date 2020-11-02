c222f37216cf43f1e4ce0ed5863f11f2f67c142c
OluwatosinOA <73297515+OluwatosinOA@users.noreply.github.com>
2020-11-02 13:41:50 +0100

    Add files via upload

---
+++ EXAM_GT_CSHARP_CLASS_QUESTION5/Dtos/Country/CountryRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASSESSMENT_QUESTION5.Dtos.Country
+{
+    public class CountryRequest
+    {
+        public string CountryName { get; set; }
+    }
+}
---
+++ EXAM_GT_CSHARP_CLASS_QUESTION5/Dtos/ReturnResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASSESSMENT_QUESTION5.Dtos
+{
+    public class ReturnResponse
+    {
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+        public object ObjectValue { get; set; }
+    }
+}
